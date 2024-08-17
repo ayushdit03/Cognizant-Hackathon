5400f48d4437792a43298baf28a801d6a4313349
ayushdit03 <21512914.dit@dypvp.edu.in>
2024-08-17 23:50:47 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import streamlit as st
+HORIZONTAL_RED = "E:\\Nikhil\\Cognizant\\main2\\images\\img7.jpg"
+ICON_RED = "E:\\Nikhil\\Cognizant\\main2\\images\\img6.jpg"
+
+# options = [HORIZONTAL_RED, ICON_RED]
+# sidebar_logo = st.selectbox("Sidebar logo", options, 0)
+# main_body_logo = st.selectbox("Main body logo", options, 1)
+
+# st.logo(sidebar_logo, icon_image=main_body_logo)
+# st.sidebar.markdown("Hi!")
+
+st.logo(ICON_RED)
